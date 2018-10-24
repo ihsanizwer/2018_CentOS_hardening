e1e0fbd6668458ed7823011a2723872c2c2988ce
ihsanizwer <ihsanizwer@yahoo.com>
2018-10-24 13:40:13 +0530

    Corrected errors in 1-2-2

--- units/1-2-2.sh
+++ units/1-2-2.sh
@@ -1,16 +1,16 @@
 echo "Ensure GPG keys are configured"
-	target1=`grep -n "^enabled=1" /etc/yum.d/sublime-text.repo`
-	target2=`grep -n "^enabled=1" /etc/yum.d/virtualbox.repo`
-	target3=`grep -n "^enabled=1" /etc/yum.d/virtualbox.repo.1`
-	target4=`grep -n "^enabled=1" /etc/yum.d/google-chrome.repo`
+	target1=`grep -n "^enabled=1" /etc/yum.repos.d/sublime-text.repo | cut -f1 -d:`
+	target2=`grep -n "^enabled=1" /etc/yum.repos.d/virtualbox.repo | cut -f1 -d: `
+	target3=`grep -n "^enabled=1" /etc/yum.repos.d/virtualbox.repo.1 | cut -f1 -d:`
+	target4=`grep -n "^enabled=1" /etc/yum.repos.d/google-chrome.repo | cut -f1 -d:`
 
-	sed -i ${target1}'s/.*/enabled=0/' /etc/yum.d/sublime-text.repo
+	sed -i ${target1}'s/.*/enabled=0/' /etc/yum.repos.d/sublime-text.repo
 	check1=$?
-	sed -i ${target2}'s/.*/enabled=0/' /etc/yum.d/virtualbox.repo
+	sed -i ${target2}'s/.*/enabled=0/' /etc/yum.repos.d/virtualbox.repo
 	check2=$?
-	sed -i ${target3}'s/.*/enabled=0/' /etc/yum.d/virtualbox.repo.1
+	sed -i ${target3}'s/.*/enabled=0/' /etc/yum.repos.d/virtualbox.repo.1
 	check3=$?
-	sed -i ${target4}'s/.*/enabled=0/' /etc/yum.d/google-chrome.repo
+	sed -i ${target4}'s/.*/enabled=0/' /etc/yum.repos.d/google-chrome.repo
 	check4=$?
 
 	if([ $check1 -eq 0 ] && [ $check2 -eq 0 ] && [ $check3 -eq 0 ] && [ $check4 -eq 0 ]);then
